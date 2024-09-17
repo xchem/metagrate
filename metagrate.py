657fdfa8e97da92e2a7068d98ab61edf9eea7e02
Max Winokan <mwinokan@me.com>
2024-09-17 10:57:51 +0100

    rename XCA aliases

--- metagrate.py
+++ metagrate.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from pathlib import Path
 from argparse import ArgumentParser
+from fnmatch import fnmatch
 # import mcol
 
 from mlog import setup_logger
@@ -15,6 +16,8 @@ C_SMILES = 'Smiles'
 
 CURATOR_TAG_CATEGORIES = ['Other', 'Forum', 'Series']
 SITE_TAG_TYPES = ['ConformerSites', 'CanonSites', 'CrystalformSites', 'Crystalforms', 'Quatassemblies']
+
+SITE_TAG_CACHE = { k:{} for k in SITE_TAG_TYPES }
 
 def parse_args():
 
@@ -31,8 +34,6 @@ def load_csv(path):
 
 	if 'Pose' not in df.columns:
 		logger.warning(f'Old metadata format: {path}')
-
-	# print(df.columns)
 
 	return df
 
@@ -75,25 +76,105 @@ def get_curator_tags(row):
 
 def compare_site_tags(source, template):
 
+	global SITE_TAG_CACHE
+
 	for site_type in SITE_TAG_TYPES:
 
-		logger.var(site_type, (source[site_type], template[site_type]))
+		cache = SITE_TAG_CACHE[site_type]
+
+		col = f'{site_type} alias'
+
+		source_value = remove_tag_prefix(source[col])
+		template_value = remove_tag_prefix(template[col])
+		cache_value = cache[template_value] if template_value in cache else None
+
+		if cache_value and cache_value != source_value:
+			logger.var(col, (source_value, template_value, cache_value))
+			logger.error(f'{col} inconsistency!')
+
+		elif cache_value is None:
+			cache[template_value] = source_value
+
+def remove_tag_prefix(tag):
+	return tag.split(' - ')[1]
+
+def detect_generated_site_alias(site_type, alias):
+
+	match site_type:
+		case 'ConformerSites':
+			if fnmatch(alias, '*-x[0-9][0-9][0-9][0-9]'):
+				return True
+			elif fnmatch(alias, '*[0-9][0-9][0-9][0-9]/*/*'):
+				return True
+
+		case 'CanonSites':
+			if fnmatch(alias, '*-x[0-9][0-9][0-9][0-9]/*/*/*'):
+				return True
+			elif fnmatch(alias, '*[0-9][0-9][0-9][0-9]/*/*/*'):
+				return True
+
+		case 'CrystalformSites':
+			if fnmatch(alias, '*-x[0-9][0-9][0-9][0-9]/*/*'):
+				return True
+			elif fnmatch(alias, '*[0-9][0-9][0-9][0-9]/*/*'):
+				return True
+
+		case 'Crystalforms':
+			if fnmatch(alias, '*_*_*'):
+				return True
+			elif fnmatch(alias, '*/*/*'):
+				return True
+
+		case 'Quatassemblies':
+			return False
+
+		case _:
+			raise NotImplementedError(f'detect_generated_site_alias({site_type=})')
+
+	return False
+
+def apply_generated_site_aliases(df):
+
+	del_list = []
+
+	for site_type in SITE_TAG_CACHE:
+		for old,new in SITE_TAG_CACHE[site_type].items():
+			is_generated = detect_generated_site_alias(site_type,new)
+			
+			if is_generated:
+				del_list.append((site_type, old))
+
+			elif old == new:
+				del_list.append((site_type, old))
+	
+	for site_type, old in del_list:	
+		del SITE_TAG_CACHE[site_type][old]
+
+	for site_type in SITE_TAG_CACHE:
+		for old,new in SITE_TAG_CACHE[site_type].items():
+			
+			col = f'{site_type} alias'
+
+			subset = df[df[col].str.endswith(old)]
+			
+			prefix = subset[col].values[0].split(' - ')[0]
+
+			df.loc[subset.index, col] = f'{prefix} - {new}'
+
+			logger.var(f'Renamed {site_type} alias',f'{old} --> {new}')
 
 def migrate_tags(source, template):
 
 	df = template.copy()
 
-	# df = template
-
 	curator_tags = {}
 
 	for i,row in df.iterrows():
 
-		logger.debug((row[C_SHORTCODE], row[C_LONGCODE]))
-
 		reference = match_row_to_source(row, source)
 
 		# check XCA tags
+		compare_site_tags(reference, row)
 
 		# curator tags:
 		tags = get_curator_tags(reference)
@@ -105,6 +186,7 @@ def migrate_tags(source, template):
 	# apply curator tags
 	for col, values in curator_tags.items():
 		df[col] = values
+		logger.var('Migrated curator tag', col)
 
 	return df
 
@@ -124,6 +206,8 @@ def main():
 	df2 = load_csv(template)
 
 	df3 = migrate_tags(df1, df2)
+
+	apply_generated_site_aliases(df3)
 
 	logger.writing(output)
 	df3.to_csv(output, index=False)
